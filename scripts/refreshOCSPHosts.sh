51984266bf84101377f20043ecd711bff42cfe5c
Jauder Ho <jauderho@users.noreply.github.com>
2024-07-24 22:03:38 +0000

    Add list of OCSP hosts
    Signed-off-by: Jauder Ho <jauderho@users.noreply.github.com>

---
+++ scripts/refreshOCSPHosts.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Fetch known OCSP hosts from https://crt.sh/
+#
+# Run this at the base directory of your repo
+# Make sure to configure .github/dependabot.yml to update "github-actions" in a timely manner
+# This may break your setup. Read carefully before running
+#
+set -euo pipefail
+IFS=$'\n\t'
+
+# make sure we have the latest commits
+git pull
+
+# fetch from crt.sh
+# only parse the lines with potential OCSP hosts
+# extract the content
+# cleanup to only retain hostnames
+# drop bad entries
+# drop IP addresses
+# normalize
+# sort then uniq
+# output in hosts format
+curl -sL https://crt.sh/ocsp-responders | \
+grep "A title" | \
+sed -n 's/.*<A[^>]*>\([^<]*\)<\/A>.*/\1/p' | \
+sed -e 's#https\?://##' -e 's#hhtp://##' -e 's#:[0-9]*##' -e "s#/.*##" | \
+grep -iEv "ldap:|url=|uri=|uri:" | \
+grep -v "\.$" | \
+grep -Ev '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' | \
+tr '[:upper:]' '[:lower:]' | \
+tr -cd '[:alnum:]-.\n' | \
+sort -u | \
+sed -e 's#^#0.0.0.0 #' > ocsp-hosts
+
